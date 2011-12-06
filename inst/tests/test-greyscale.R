f9a7adc8b6c21257aa8340172f7f288d6a1529d0
Gregory Jefferis <jefferis@gmail.com>
2011-12-06 21:41:57 +0000

    Add tests for simple 8 bit and RGB images

---
+++ inst/tests/test-greyscale.R
@@ -0,0 +1,18 @@
+context("Reading simple greyscale images")
+
+require(pixmap)
+
+test_that("8 bit bmp image matches pgm equivalent loaded with pixmap", {
+      pgmfile="../images/w5h3-8bit.pgm"
+      bmpfile=sub("pgm$","bmp",pgmfile)
+      expect_that(read.bmp(bmpfile)/(2^8-1),
+          is_equivalent_to(read.pnm(pgmfile)@grey))
+    })
+
+# don't have an example of this
+#test_that("16 bit bmp image matches pgm equivalent loaded with pixmap", {
+#      pgmfile="../images/w5h3-16bit.pgm"
+#      bmpfile=sub("pgm$","bmp",pgmfile)
+#      expect_that(read.bmp(bmpfile)/(2^16-1),
+#          is_equivalent_to(read.pnm(pgmfile)@grey))
+#    })
